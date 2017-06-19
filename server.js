2ffba05f3daf56ff4fdb983d93935d41f5970844
Pasakorn Promsuban <pasakorn.p@beryl8.com>
2017-06-19 17:50:44 +0700

    Refractored to using message model - Todo refractor to promise

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ let io = require('socket.io')(http);
 //   ]
 // }
 
+//Need global users
+
 //Need a way to know which is pc and which is tablet in a room;
 const rowCounter = 'RowCounter';
 const customer = 'Customer';
@@ -22,21 +24,37 @@ const noRoom = 'No room with the roomId';
 const roomFull = 'Full';
 var rooms = [];
 
+// Use promise
+
+
 io.on('connection', (socket) => {
   console.log('USER CONNECTED');
-  console.log(socket.id);
 
-  socket.on('joinRoom', (role,roomId) => {
 
+
+  socket.on('message', (message) => {
+    if(message.component == 'joinRoom'){
+      var role = message.message.role;
+      var roomId = message.message.roomId;
+      checkNumOfUsersInRoom(socket,role,roomId);
+
+    }else{
+      socket.broadcast.to(message.clientId).emit('message',message);
+    }
+  });
+
+  socket.on('disconnect', ()=> {
+    console.log('User disconnected');
+  });
+});
+
+function checkNumOfUsersInRoom(socket,role, roomId){
     // Check if room full
     // If first member in room then must be row counter
     // If second member then must be customer
     var clients = io.sockets.adapter.rooms[roomId];
     var clientToSendTo;
-
-    // Object.keys(clients.sockets).length < 2
-    console.log(typeof role);
-    console.log(role);
+    var message;
 
     if(typeof clients === 'undefined' && role === rowCounter){
       socket.nickname = rowCounter;
@@ -50,32 +68,38 @@ io.on('connection', (socket) => {
       var rowCounterId = Object.keys(clients)[0];
       var customerId = Object.keys(clients)[1];
 
-      socket.emit('setClientToSendTo',rowCounterId);
-      socket.broadcast.to(rowCounterId).emit('setClientToSendTo',customerId);
+      setClientToSendTo(socket,rowCounterId);
+      setClientToSendTo(socket.broadcast.to(rowCounterId),customerId);
+
     }else if(role === customer && typeof clients === 'undefined'){
       console.log('invalid room');
-      socket.emit('getRoomStatus',noRoom);
+
+      sendRoomStatus(socket,noRoom);
       socket.disconnect();
     }else{
       console.log('room full');
-      socket.emit('getRoomStatus',roomFull);
+
+      sendRoomStatus(socket,roomFull);
       socket.disconnect();
     }
+}
 
-    ///////////////////////////////////////////////////////////////////////////
+function setClientToSendTo(socket,idToSend){
+  message = {
+    component: 'setClientToSendTo',
+    message: idToSend
+  };
+  socket.emit('message',message);
+}
 
-    socket.on('message', (clientToSendToId,message) => {
-      socket.broadcast.to(clientToSendToId).emit('message',message);
-    });
+function sendRoomStatus(socket,status){
+  message = {
+    component: 'getRoomStatus',
+    message: status
+  };
+  socket.emit('message',message);
+}
 
-    socket.on('disconnect', function(){
-      console.log('USER DISCONNECTED');
-    });
-
-    ///////////////////////////////////////////////////////////////////////////
-  });
-});
-
-http.listen(8080, '192.168.1.178', () => {
+http.listen(8080, '172.20.10.4', () => {
   console.log('started on port 8080');
 });
