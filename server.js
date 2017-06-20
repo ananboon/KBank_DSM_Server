d43bba9f3fea4f0a1d763ff2aee36dc72603c766
Pasakorn Promsuban <pasakorn.p@beryl8.com>
2017-06-20 17:55:19 +0700

    Used mysql for database connection - TODO change callbacks to promise

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 'use strict';
-
 let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+let mysql = require('mysql');
+let Promise = require('bluebird');
 
 
-// Need to change to Sessions
-// _global.ses = {
-//   users:[
-//     {
-//       "id":"user1"
-//     }
-//   ]
-// }
+let dbUsername = 'root';
+let dbPassword = 'root';
+let dbHost = 'localhost';
+let databaseKbank = 'kbank_dsm';
 
-//Need global users
+var pool = mysql.createPool({
+  connectionLimit: 1000,
+  host: dbHost,
+  user: dbUsername,
+  password: dbPassword,
+  database: databaseKbank
+});
+
+// Mock users
+// global users = [];
 
 //Need a way to know which is pc and which is tablet in a room;
 const rowCounter = 'RowCounter';
@@ -22,22 +28,49 @@ const customer = 'Customer';
 
 const noRoom = 'No room with the roomId';
 const roomFull = 'Full';
-var rooms = [];
 
 // Use promise
-
-
 io.on('connection', (socket) => {
   console.log('USER CONNECTED');
 
-
-
   socket.on('message', (message) => {
-    if(message.component == 'joinRoom'){
+    if(message.component === 'login'){
+      var role = message.message.role;
+      var username = message.message.username;
+      var password = message.message.password;
+
+      pool.getConnection((error,connection) => {
+        if(error){
+          console.log('there is an error to the database connection');
+          return ;
+        }else{
+          var query = 'SELECT Id, FirstName, LastName, Position, Location FROM Users WHERE Username = ? AND Password = ?';
+          var values = [username,password];
+          var statement = mysql.format(query,values);
+
+          connection.query(statement,(error,rows) => {
+            if(error){
+              console.log('error querying');
+            }else{
+              message = {
+                component: 'user-data',
+                message: {
+                  id: rows[0].id,
+                  firstname: rows[0].firstname,
+                  lastname: rows[0].lastname,
+                  position: rows[0].position,
+                  location: rows[0].location
+                }
+              };
+
+            }
+          });
+        }
+      });
+    }else if(message.component === 'joinRoom'){
       var role = message.message.role;
       var roomId = message.message.roomId;
       checkNumOfUsersInRoom(socket,role,roomId);
-
     }else{
       socket.broadcast.to(message.clientId).emit('message',message);
     }
@@ -72,13 +105,9 @@ function checkNumOfUsersInRoom(socket,role, roomId){
       setClientToSendTo(socket.broadcast.to(rowCounterId),customerId);
 
     }else if(role === customer && typeof clients === 'undefined'){
-      console.log('invalid room');
-
       sendRoomStatus(socket,noRoom);
       socket.disconnect();
     }else{
-      console.log('room full');
-
       sendRoomStatus(socket,roomFull);
       socket.disconnect();
     }
@@ -100,6 +129,6 @@ function sendRoomStatus(socket,status){
   socket.emit('message',message);
 }
 
-http.listen(8080, '172.20.10.4', () => {
+http.listen(8080, '192.168.1.182', () => {
   console.log('started on port 8080');
 });
